d8517dcf104b576727b6a61e475953a1de04d040
Abhishek Mishra <abhi.mishra509@gmail.com>
2022-08-01 15:31:20 +0530

    initial commit

---
+++ for_loop/Q1.sh
@@ -0,0 +1,8 @@
+read -p "enter the number - " num
+echo "print power of 2 : "
+for ((i=1; i<=$num; i++))
+do 
+echo $((2*$i))
+done
+
+
---
+++ for_loop/Q2.sh
@@ -0,0 +1,9 @@
+read -p "enter any number" n
+float sum=0
+n= Integer.parseInt(s[0]);
+
+for (i=1; i<=n;i++)
+
+sum=sum+(float)1/i
+echo "sum=+sum"
+done
